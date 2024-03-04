b044d3f6e3e8db0e79fc9c14f0e1c19ce01a1145
David Ulrich <dulrich@c4r3p3.42vienna.com>
2024-03-04 16:45:42 +0100

    new gnl seems to leak quite a bit of memory

---
+++ get_next_line.c
@@ -0,0 +1,126 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+int	ft_strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *str, int c)
+{
+	while (*str) 
+	{
+		if (*str == (char)c)
+			return ((char *)str);
+	}
+	return (NULL);
+
+}
+
+int	ft_strlcpy(char *dest, char *src, int dest_size)
+{
+	int i = 0;
+	int src_size = ft_strlen(src);
+
+	if (dest_size > 0)
+	{
+		while (i < src_size && i < dest_size - 1)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (src_size);
+}
+
+char	*ft_strdup(char *str)
+{
+	char	*dup;
+	int		len;
+
+	if (!str)
+		return (NULL);
+	len = ft_strlen(str) + 1;
+	dup = malloc(len * sizeof(char));
+	if (!dup)
+		return (NULL);
+	ft_strlcpy(dup, str, len);
+	return (dup);
+}
+
+char	*ft_strjoin(char *s1, char *s2, int len)
+{
+	int		s1_len;
+	int		s2_len;
+	char	*joined;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = len;
+	joined = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (!joined)
+		return (NULL);
+	ft_strlcpy(joined, s1, s1_len + 1);
+	ft_strlcpy((joined + s1_len), s2, s1_len + 1);
+	return (joined);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buf[BUFFER_SIZE + 1];
+	char		*line;
+	char		*newline;
+	int			count;
+	int			to_copy;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line = ft_strdup(buf);
+	while (!ft_strchr(line, '\n') && (count = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		buf[count] = '\0';
+		line = ft_strjoin(line, buf, count);
+	}
+	if (ft_strlen(line) == 0)
+		return (free(line), NULL);
+	newline = ft_strchr(line, '\n');
+	if (newline)
+	{
+		to_copy = newline - line + 1;
+		ft_strlcpy(buf, newline + 1, BUFFER_SIZE + 1);
+	}
+	else
+	{
+		to_copy = ft_strlen(line);
+		ft_strlcpy(buf, "", BUFFER_SIZE + 1);
+	}
+	line[to_copy] = '\0';
+	return (line);
+}
+
+int main(void)
+{
+	//int lines = 1;
+	int fd = open("../testing/lorem.txt", O_RDONLY);
+	char *line = get_next_line(fd);
+	printf("%s", line);
+
+	/* while ((line = get_next_line(fd)))
+	{
+		printf("%d->%s", lines++, line);
+		free(line);
+	} */
+	return (0);
+}
