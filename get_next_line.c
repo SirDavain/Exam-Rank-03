d56323e4d805613bd31c5bd2e9431ee601bb0150
David Ulrich <dulrich@c4r3p5.42vienna.com>
2024-03-06 15:14:08 +0100

    ft_printf and gnl are leak-free and accepted by grademe.fr

--- get_next_line.c
+++ get_next_line.c
@@ -1,11 +1,4 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
-# endif
+#include "get_next_line.h"
 
 int	ft_strlen(char *str)
 {
@@ -22,9 +15,9 @@ char	*ft_strchr(char *str, int c)
 	{
 		if (*str == (char)c)
 			return ((char *)str);
+		str++;
 	}
 	return (NULL);
-
 }
 
 int	ft_strlcpy(char *dest, char *src, int dest_size)
@@ -34,7 +27,7 @@ int	ft_strlcpy(char *dest, char *src, int dest_size)
 
 	if (dest_size > 0)
 	{
-		while (i < src_size && i < dest_size - 1)
+		while ((i < src_size) && (i < dest_size - 1))
 		{
 			dest[i] = src[i];
 			i++;
@@ -49,8 +42,6 @@ char	*ft_strdup(char *str)
 	char	*dup;
 	int		len;
 
-	if (!str)
-		return (NULL);
 	len = ft_strlen(str) + 1;
 	dup = malloc(len * sizeof(char));
 	if (!dup)
@@ -73,7 +64,8 @@ char	*ft_strjoin(char *s1, char *s2, int len)
 	if (!joined)
 		return (NULL);
 	ft_strlcpy(joined, s1, s1_len + 1);
-	ft_strlcpy((joined + s1_len), s2, s1_len + 1);
+	ft_strlcpy((joined + s1_len), s2, s2_len + 1);
+	free(s1);
 	return (joined);
 }
 
@@ -88,7 +80,7 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	line = ft_strdup(buf);
-	while (!ft_strchr(line, '\n') && (count = read(fd, buf, BUFFER_SIZE)) > 0)
+	while (!(ft_strchr(line, '\n')) && (count = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
 		buf[count] = '\0';
 		line = ft_strjoin(line, buf, count);
@@ -113,9 +105,11 @@ char	*get_next_line(int fd)
 int main(void)
 {
 	//int lines = 1;
-	int fd = open("../testing/lorem.txt", O_RDONLY);
+	int fd = open("lorem.txt", O_RDONLY);
 	char *line = get_next_line(fd);
 	printf("%s", line);
+	free(line);
+	close(fd);
 
 	/* while ((line = get_next_line(fd)))
 	{
